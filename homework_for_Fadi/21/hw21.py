f99feca58b7733052bb715b57b4cce8c31d89355
sofiiaboklan <124063273+sofiiaboklan@users.noreply.github.com>
2023-03-14 22:38:41 +0000

    uploaded hw21

---
+++ homework_for_Fadi/21/hw21.py
@@ -0,0 +1,67 @@
+from typing import List, Tuple
+
+
+# We assume that all lists passed to functions are the same length N
+
+
+# answers
+# 1 - n
+# 2 - 1
+# 3 - n^2
+# 4 - n
+# 5 - n^2
+# 6 - log n
+
+
+# n^2 - цикл у циклі: for та if
+def question1(first_list: List[int], second_list: List[int]) -> List[int]:
+    res: List[int] = []
+    for el_first_list in first_list:
+        if el_first_list in second_list:
+            res.append(el_first_list)
+    return res
+
+# 1 - бо маємо константу 10
+def question2(n: int) -> int:
+    for _ in range(10):
+        n **= 3
+    return n
+
+
+# log n - є додаткова умова, яка накладається на цикл і зменшує кількість елементів які ми розглядаємо
+def question3(first_list: List[int], second_list: List[int]) -> List[int]:
+    temp: List[int] = first_list[:]
+    for el_second_list in second_list:
+        flag = False
+        for check in temp:
+            if el_second_list == check:
+                flag = True
+                break
+        if not flag:
+            temp.append(second_list)
+    return temp
+
+
+# n - один цикл і одна умова
+def question4(input_list: List[int]) -> int:
+    res: int = 0
+    for el in input_list:
+        if el > res:
+            res = el
+    return res
+
+
+# n^2 - цикл у циклі
+def question5(n: int) -> List[Tuple[int, int]]:
+    res: List[Tuple[int, int]] = []
+    for i in range(n):
+        for j in range(n):
+            res.append((i, j))
+    return res
+
+
+# n - один цикл
+def question6(n: int) -> int:
+    while n > 1:
+        n /= 2
+    return n
